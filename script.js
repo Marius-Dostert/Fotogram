c6627921af09b9661a8e3b745c7f92dc63c3d994
Marius <marius.dostert@web.de>
2026-02-02 17:02:32 +0100

    Added the Fotogram Project (index.html, script.js, style.css and all icons and images.

---
+++ script.js
@@ -0,0 +1,44 @@
+
+let ContentArray = ["content_box_one", "content_box_two", "content_box_three", "content_box_four", "content_box_five", "content_box_six", "content_box_seven", "content_box_eight", "content_box_nine", "content_box_ten", "content_box_eleven", "content_box_twelve"];
+
+function open_with_enter(usedKey, content_box_number) {
+    let key = usedKey
+    if 
+    (key === 'Enter') {open_content_box(content_box_number)}
+}
+
+function open_content_box(content_box_number){
+    let dialogRef = document.getElementById(ContentArray [content_box_number]);
+    dialogRef.showModal();
+}
+
+function close_content_box(content_box_number){
+    let dialogRef = document.getElementById(ContentArray [content_box_number]);
+    dialogRef.close();
+}
+
+function switch_content(content_box_number) {
+    let dialogRef = document.getElementById(ContentArray[content_box_number]);
+    dialogRef.showModal();
+}
+
+function previous_content(toClose, openPrevious) {
+    close_content_box(toClose)
+    switch_content(openPrevious)
+}
+
+function next_content(toClose, openNext) {
+    close_content_box(toClose)      
+    switch_content(openNext)
+}
+
+function ArrowNavigation(event, openPrevious, toClose, openNext) {
+    if
+    (event.key === 'ArrowLeft') {switch_content(openPrevious), close_content_box(toClose)} 
+    else if
+    (event.key === 'ArrowRight') {switch_content(openNext), close_content_box(toClose)}
+}
+
+function bubbelingProtection (event) {
+    event.stopPropagation()
+}
