2fe0a6817b299c27fc405e09e94e581f8f562884
Marius <marius.dostert@web.de>
2026-02-16 15:33:54 +0100

    Added onload render and content div in html. Added for loop, contetnTitles array and contentImg array in scripts.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+let contentTitles = ["Merkur", "Venus", "Erde", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun"]
+
+let contentImg = ["img src='content/Mercury.jpg'"]
+
+function render () {
+    let contentref = document.getElementById("content")
+    for (let index = 0; index < contentTitles.length; index++) {
+        contentref.innerHTML += getNotesHtml(index);
+    }
+}
+
+function getNotesHtml(index){
+    return `     <div class="single_element">
+                                        <h1>
+                                            ${contentTitles[index]}
+                                        </h1>
+                                        <p>
+                                            ${contentImg[index]}
+                                        </p>
+                                    </div>`
+}
 
 
 let ContentArray = ["content_box_one", "content_box_two", "content_box_three", "content_box_four", "content_box_five", "content_box_six", "content_box_seven", "content_box_eight", "content_box_nine", "content_box_ten", "content_box_eleven", "content_box_twelve"];
