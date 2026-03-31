b6443cf1f171ef62cdc5422c4ed71aede77ac58a
Marius <marius.dostert@web.de>
2026-03-31 18:59:34 +0200

    deleted the open_with_enter() function since it isn't nececary anymore. fixed a bug that the first thumbnail could not open the first dialog.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let contentThumbnail = [`<button class="thumbnail_button" onclick="open_content_box(0)" onkeydown="open_with_enter(event.key, 0)"><img src="content/Mercury.jpg" class="content_box_design" alt="Merkur"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(1)" onkeydown="open_with_enter(event.key, 1)"><img src="content/Venus.png" class="content_box_design" alt="Venus"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(2)" onkeydown="open_with_enter(event.key, 2)"><img src="content/Earth.jpg" class="content_box_design" alt="Erde"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(3)" onkeydown="open_with_enter(event.key, 3)"><img src="content/Mars.jpg" class="content_box_design" alt="Mars"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(4)" onkeydown="open_with_enter(event.key, 4)"><img src="content/Jupiter.png" class="content_box_design" alt="Jupiter"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(5)" onkeydown="open_with_enter(event.key, 5)"><img src="content/Jupiter_moons.jpg" class="content_box_design" alt="Jupiter und die Galileischen Monde"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(6)" onkeydown="open_with_enter(event.key, 6)"><img src="content/Saturn.jpg" class="content_box_design" alt="Saturn"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(7)" onkeydown="open_with_enter(event.key, 7)"><img src="content/Saturn_ring.jpg" class="content_box_design" alt="Ringe des Saturn"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(8)" onkeydown="open_with_enter(event.key, 8)"><img src="content/Uranus.png" class="content_box_design" alt="Uranus"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(9)" onkeydown="open_with_enter(event.key, 9)"><img src="content/Uranus_sunrise.jpg" class="content_box_design" alt="Dämmerung auf Uranus"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(10)" onkeydown="open_with_enter(event.key, 10)"><img src="content/neptun.jpg" class="content_box_design" alt="Neptun"></button>
-                         <button class="thumbnail_button" onclick="open_content_box(11)" onkeydown="open_with_enter(event.key, 11)"><img src="content/Neptune_storms.jpg" class="content_box_design" alt="Stürme auf Neptun"></button>
+let contentThumbnail = [`<button class="thumbnail_button" onclick="open_content_box(0)"><img src="content/Mercury.jpg" class="content_box_design" alt="Merkur"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(1)"><img src="content/Venus.png" class="content_box_design" alt="Venus"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(2)"><img src="content/Earth.jpg" class="content_box_design" alt="Erde"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(3)"><img src="content/Mars.jpg" class="content_box_design" alt="Mars"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(4)"><img src="content/Jupiter.png" class="content_box_design" alt="Jupiter"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(5)"><img src="content/Jupiter_moons.jpg" class="content_box_design" alt="Jupiter und die Galileischen Monde"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(6)"><img src="content/Saturn.jpg" class="content_box_design" alt="Saturn"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(7)"><img src="content/Saturn_ring.jpg" class="content_box_design" alt="Ringe des Saturn"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(8)"><img src="content/Uranus.png" class="content_box_design" alt="Uranus"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(9)"><img src="content/Uranus_sunrise.jpg" class="content_box_design" alt="Dämmerung auf Uranus"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(10)"><img src="content/neptun.jpg" class="content_box_design" alt="Neptun"></button>
+                         <button class="thumbnail_button" onclick="open_content_box(11)"><img src="content/Neptune_storms.jpg" class="content_box_design" alt="Stürme auf Neptun"></button>
                         `]
 
 let contentHTML = [ `<dialog id = dialog_0 onclick="close_content_box(0)">
@@ -214,11 +214,6 @@ function getNotesHtml(index){
 }
 
 
-function open_with_enter(usedKey, content_box_number) {
-    let key = usedKey
-    if 
-    (key === 'Enter') {open_content_box(content_box_number)}
-}
 
 function open_content_box(content_box_number){
     let dialogRef = document.getElementById(functionIDs [content_box_number]);
