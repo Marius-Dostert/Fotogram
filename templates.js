b11d3d09cc3f1ea266c9fe9e39844cbe871ad0be
Marius <marius.dostert@web.de>
2026-08-01 11:22:49 +0200

    WCAG Reworked, added some Alt Names

--- templates.js
+++ templates.js
@@ -1,7 +1,7 @@
 function getNotesHtml(index){
     return `                <div class="thumbnail-section">
                                 <button class="thumbnail_button" onclick="open_content_box(${index})">
-                                    <img class="content_box_design" src="${dataArray[index].image}">
+                                    <img class="content_box_design" src="${dataArray[index].image}" alt="${dataArray[index].name}">
                                 </button>
                             </div>
                             <dialog id="${index}" onclick="close_content_box(${index})" onkeydown="arrowNavigation(event,${index}-1, ${index}, ${index}+1)">
@@ -14,13 +14,13 @@ function getNotesHtml(index){
                                 <footer onclick="bubbelingProtection(event)">
 
                                     <div class="button_placeholder">
-                                        <button id="previousButton${index}" tabindex="0" onclick="switch_content(${index},${index}-1)" class="dialog_navi_button" alt="zurück"><img src="img/icon/arrow-left.png"></button>
+                                        <button id="previousButton${index}" tabindex="0" onclick="switch_content(${index},${index}-1)" class="dialog_navi_button"><img src="img/icon/arrow-left.png" alt="zurück"></button>
                                     </div>
 
                                     <button onclick="close_content_box(${index})" tabindex="0" class="dialog_close_button" alt="schließen">Schließen</button>
 
                                     <div class="button_placeholder">
-                                        <button id="nextButton${index}" tabindex="0" onclick="switch_content(${index},${index}+1)" class="dialog_navi_button" alt="weiter"><img src="img/icon/arrow-right.png"></button>
+                                        <button id="nextButton${index}" tabindex="0" onclick="switch_content(${index},${index}+1)" class="dialog_navi_button"><img src="img/icon/arrow-right.png" alt="weiter"></button>
                                     </div>
 
                                 </footer>
